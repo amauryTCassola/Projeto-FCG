edee143306621aec250a63b24877705c86e0c1ad
Bruno Ramos Toresan <42551829+brunotoresan@users.noreply.github.com>
2019-06-07 21:14:50 -0300

    added intersection ray obb

--- src/IntersectionFunctions.cpp
+++ src/IntersectionFunctions.cpp
@@ -1,4 +1,5 @@
 #include "IntersectionFunctions.h"
+#include <cfloat>
 
 GLuint new_vao_id;
 GLuint id_vbo_positions;
@@ -14,14 +15,15 @@ glm::mat4 model = Matrix_Identity();
 
 bool jaGerou = false;
 
-void DesenhaCollider(){
+void DesenhaCollider()
+{
 
     glUseProgram(GPUprg);
 
     SetCameraToDraw(GPUprg, 1.0f);
 
     GLint model_matrix_index = glGetUniformLocation(GPUprg, "model");
-    glUniformMatrix4fv(model_matrix_index, 1 , GL_FALSE , glm::value_ptr(model));
+    glUniformMatrix4fv(model_matrix_index, 1, GL_FALSE, glm::value_ptr(model));
     //a primeira coisa é passar a matriz de modelagem do objeto pra GPU
 
     GLint bbox_min_uniform = glGetUniformLocation(GPUprg, "bbox_min");
@@ -109,36 +111,91 @@ bool IntersectionRayCube(glm::vec4 ray_origin, glm::vec4 ray_direction, glm::vec
     return true;
 }
 
+bool IntersectionRayOBB(glm::vec4 ray_origin, glm::vec4 ray_direction, OBB thisOBB)
+{
+    glm::vec4 X = thisOBB.eixoU;
+    glm::vec4 Y = thisOBB.eixoV;
+    glm::vec4 Z = thisOBB.eixoW;
 
-glm::vec4 ClosestPointOnOBBToPoint(const OBB obb, const glm::vec4 point) {
-     float distance = 0;
-     glm::vec4 result = obb.centro;
-     glm::vec4 dir = point - obb.centro;
+    float obb_size[] = {thisOBB.tamanhoU, thisOBB.tamanhoV, thisOBB.tamanhoW};
 
-     glm::vec4 eixos[3];
-     float tamanhos[3];
-     eixos[0] = obb.eixoU; tamanhos[0] = obb.tamanhoU*0.5f;
-     eixos[1] = obb.eixoV; tamanhos[1] = obb.tamanhoV*0.5f;
-     eixos[2] = obb.eixoW; tamanhos[2] = obb.tamanhoW*0.5f;
+    glm::vec4 p = thisOBB.centro - ray_origin;
+    glm::vec4 f = glm::vec4( dotproduct(X, ray_direction), dotproduct(Y, ray_direction), dotproduct(Z, ray_direction), 0.0f);
+    glm::vec4 e = glm::vec4(dotproduct(X, p), dotproduct(Y, p), dotproduct(Z, p), 0.0f);
 
-     for(int i = 0; i < 3; i++){
-         distance = dotproduct(dir, eixos[i]); //norma da projeção de dir sobre o eixo
+    float t[6] = { 0, 0, 0, 0, 0, 0 };
+    for (int i = 0; i < 3; ++i)
+    {
+        if( (fabsf((f[i])-(0)) <= FLT_EPSILON * fmaxf(1.0f, fmaxf(fabsf(f[i]), fabsf(0))) ))
+        {
+            if (-e[i] - obb_size[i]>0 || -e[i] + obb_size[i]<0)
+            {
+                return false;
+            }
+            f[i] = 0.00001f; // Avoid div by 0!
+        }
+        t[i * 2 + 0] = (e[i] + obb_size[i]) / f[i]; // min
+        t[i * 2 + 1] = (e[i] - obb_size[i]) / f[i]; // max
+    }
 
-         if (distance > tamanhos[i]) {
+    float tmin = fmaxf(fmaxf(
+                           fminf(t[0], t[1]),
+                           fminf(t[2], t[3])),
+                       fminf(t[4], t[5])
+                      );
+    float tmax = fminf(
+                     fminf(
+                         fmaxf(t[0], t[1]),
+                         fmaxf(t[2], t[3])),
+                     fmaxf(t[4], t[5])
+                 );
+
+    if(tmax < 0)
+        return false;
+    if(tmin > tmax)
+        return false;
+
+    return true;
+}
+
+
+glm::vec4 ClosestPointOnOBBToPoint(const OBB obb, const glm::vec4 point)
+{
+    float distance = 0;
+    glm::vec4 result = obb.centro;
+    glm::vec4 dir = point - obb.centro;
+
+    glm::vec4 eixos[3];
+    float tamanhos[3];
+    eixos[0] = obb.eixoU;
+    tamanhos[0] = obb.tamanhoU*0.5f;
+    eixos[1] = obb.eixoV;
+    tamanhos[1] = obb.tamanhoV*0.5f;
+    eixos[2] = obb.eixoW;
+    tamanhos[2] = obb.tamanhoW*0.5f;
+
+    for(int i = 0; i < 3; i++)
+    {
+        distance = dotproduct(dir, eixos[i]); //norma da projeção de dir sobre o eixo
+
+        if (distance > tamanhos[i])
+        {
             distance = tamanhos[i];
-         }
-         else if (distance < -tamanhos[i]) {
+        }
+        else if (distance < -tamanhos[i])
+        {
             distance = -tamanhos[i];
-         }
+        }
 
-         result = result + (distance*eixos[i]);
-         }
+        result = result + (distance*eixos[i]);
+    }
 
     return result;
- }
+}
 
 
-std::vector<float> IntersectionOBB_Sphere(OBB thisOBB, Sphere thisSphere){
+std::vector<float> IntersectionOBB_Sphere(OBB thisOBB, Sphere thisSphere)
+{
     std::vector<float> retorno;
 
     glm::vec4 closestPoint = ClosestPointOnOBBToPoint(thisOBB, thisSphere.centro);
@@ -147,7 +204,8 @@ std::vector<float> IntersectionOBB_Sphere(OBB thisOBB, Sphere thisSphere){
 
     //printf("\ndist: %f  raio: %f centro: %f", dist, thisSphere.raio, thisSphere.centro);
 
-    if(dist <= thisSphere.raio){
+    if(dist <= thisSphere.raio)
+    {
         retorno.push_back(closestPoint.x);
         retorno.push_back(closestPoint.y);
         retorno.push_back(closestPoint.z);
@@ -158,35 +216,27 @@ std::vector<float> IntersectionOBB_Sphere(OBB thisOBB, Sphere thisSphere){
 }
 
 
-std::vector<float> IntersectionSphereSphere(Sphere s1, Sphere s2){
+std::vector<float> IntersectionSphereSphere(Sphere s1, Sphere s2)
+{
 
     std::vector<float> retorno;
 
     glm::vec4 distance = s2.centro - s1.centro;
     float sqr_distance = dotproduct(distance, distance);
-    float sqr_sum_radius = pow((s1.raio + s2.raio) , 2);
+    float sqr_sum_radius = pow((s1.raio + s2.raio), 2);
 
     glm::vec4 distanceNorm = distance/norm(distance);
 
     glm::vec4 pontoInterseccao = s1.centro + distanceNorm*s1.raio;
 
-    if( sqr_distance >  sqr_sum_radius){
+    if( sqr_distance >  sqr_sum_radius)
+    {
         retorno.push_back(pontoInterseccao.x);
         retorno.push_back(pontoInterseccao.y);
         retorno.push_back(pontoInterseccao.z);
         retorno.push_back(pontoInterseccao.w);
     }
 
+
     return retorno;
 }
-
-
-
-
-
-
-
-
-
-
-
